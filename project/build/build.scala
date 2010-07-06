a866ac0d80ca65ee5b2077a7dd9d287dd2646f79
pedrofurla <pedrofurla@gmail.com>
2010-07-06 09:12:16 -0300

    A non-working SBT build

---
+++ project/build/build.scala
@@ -0,0 +1,24 @@
+import sbt._
+
+class Build(info: ProjectInfo) extends DefaultProject(info) {   
+  //override def compileOptions = super.compileOptions ++ Seq(ExplainTypes)
+  
+  object sources {
+	  val index = path("index") ##
+	  val testSamples = path("test") ##	  
+	  //val run = path("run")
+  }
+
+  override def outputDirectoryName = "bin"
+  
+  override def mainScalaSourcePath = "index"
+  
+  val dep = "org.apache.ant" % "ant" % "1.8.0"
+  val dep2 = "org.scala-lang" % "scala-compiler" % "2.8.0.RC7"
+  override def filterScalaJars = false
+  
+  //val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+  
+  val mavenLocal = "Local Maven Repository" at "file://"+"C:/java/maven/repo"
+
+}
