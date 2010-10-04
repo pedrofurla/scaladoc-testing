3948c637d6b0b57debd2fed6fada8c2b0a529121
pedrofurla <pedrofurla@gmail.com>
2010-10-04 01:08:16 -0300

    Hacking the REPL to run the tests for me.

--- util/util/DocCompiler.scala
+++ util/util/DocCompiler.scala
@@ -1,6 +1,9 @@
 package util
 
 import scala.tools.nsc.io._
+import scala.tools.nsc.Global
+import scala.tools.nsc.doc
+import scala.tools.nsc.doc.model.comment._
 
 class DocCompiler(val files:List[File]) {
   import scala.tools.nsc.reporters.{Reporter, ConsoleReporter}
@@ -27,6 +30,23 @@ class DocCompiler(val files:List[File]) {
   }
       
 }
+
+class ModelFactoryMock(val g: Global, val s: doc.Settings)
+  extends doc.model.ModelFactory(g, s) {
+  thisFactory: ModelFactoryMock with CommentFactory with doc.model.TreeFactory =>
+
+  def strip(c: Comment): Option[Inline] = {
+    c.body match {
+      case Body(List(Paragraph(Chain(List(Summary(inner)))))) => Some(inner)
+      case _ => None
+    }
+  }
+
+  def parseRaw(s:String) = parse(s, "", scala.tools.nsc.util.NoPosition)
+  def parseComment(s: String): Option[Inline] =
+    strip(parse(s, "", scala.tools.nsc.util.NoPosition))
+}
+
 object Util {
 	/** Given a directory recursivelly returns the list of files ending with `.scala`. 
 	 *  Directories named `.git` or `.svn` are ignored. */
@@ -36,10 +56,46 @@ object Util {
 	
 	def scalaFiles(dir:String):List[File] = scalaFiles(Path(dir).toDirectory)  
 	
-	def docUniverse(files:List[File]) = new DocCompiler(files).docUniverse 
+	def docUniverse(files:List[File]) = new DocCompiler(files).docUniverse
+	
+	def scriptRun(args:String*) = MainGenericRunner.main(args.toArray[String])
+	
+	lazy val modelFactory = {
+	import scala.tools.nsc.doc.model._
+    val settings = new doc.Settings((str: String) => {})
+    val reporter = new scala.tools.nsc.reporters.ConsoleReporter(settings)
+    val g = new Global(settings, reporter)
+    (new ModelFactoryMock(g, settings) with CommentFactory with doc.model.TreeFactory)
+  }
+	
+	def runTests(testRoot:String) { // not working, it's using scala.home classpath
+		System.setProperty("scala.home","C:/dev/langs/scala/scala-2.8.0")
+		scalaFiles(testRoot).foreach { f =>
+			try {
+				val file = f.toAbsolute.toString
+				println("Running "+file) // "-howtorun:object"
+				scriptRun("-nocompdaemon","-i",file,"-e","Test.main(Array.empty)")
+			} catch {
+				case x : Error => println(x)
+			}
+		}
+	}
 }
+
 object Run {
 	def main(args:Array[String]) = {
-		
+		val factory = Util.modelFactory;
+		val doc = """
+				|/**
+				| * blabla
+				| *
+				| * @version bla
+				| * @author Fulano
+				| * @author Bleh
+				| */
+				""".stripMargin
+		val doc2 = "/** @author Fulano */"
+		println(factory.parseRaw(doc))
+		println(factory.parseRaw(doc).authors)
 	}
 }
